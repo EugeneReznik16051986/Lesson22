d6cadfa38b1b233cf297bc6c16a7740a6782afb2
EugeneReznik16051986 <50756243+EugeneReznik16051986@users.noreply.github.com>
2019-08-01 18:03:54 +0300

    Add files via upload

---
+++ Lesson22/script22.js
@@ -0,0 +1,58 @@
+"use strict";
+
+
+// № 1
+/*
+1. Дан массив с числами (Пример: var arr = [2, -5, 1, -2, 0, -3, 2, -14]) - массив может быть и другим.
+1) В начале вам нужно убрать из него все отрицательные числа используя filter
+2) Извлечь корень из каждого значения этого массива и записать в переменную. Используя map.
+3) Вывести массив в консоль*/
+
+let arr = [2, -5, 1, -2, 0, -3, 2, -14];
+let arr2 = arr.map(function(summ){
+  return Math.sqrt( summ );
+});
+function Array(number){
+    return number >= 0;
+}
+let positiveArray = arr.filter(Array);
+console.log( positiveArray );
+console.log( arr2 );
+
+
+
+
+
+// № 2
+/*
+2. Дан массив с числами (Пример: var arr = [4, 3, 5, 5, 0, 4, 8, 6]) - массив может быть и другим.
+Нужно найти сумму элементов от начала и до нуля. (В примере это первые 4 элемента)
+Использовать функция reduce
+*/
+
+let array = [ 4, 3, 5, 5, 0, 4, 8, 6 ];
+let summ = 0;
+array.reduce( function ( a , b ) {
+  if ( b === 0 ) {
+    summ = a;
+    return a;
+  }
+  return a + b;
+})
+console.log( summ );
+
+
+// № 3
+/*
+3. Напишите функцию summArguments, которая сложит все аргументы переданные в функцию. Например
+summArguments(10, 1, 2, 5); // 18
+summArguments(10); // 10
+*/
+function summArguments(){
+  arguments.reduce = [].reduce;
+  return arguments.reduce(function(a, b) {
+    return a + b;
+  });
+}
+console.log( summArguments(10, 1, 2, 5) );
+
